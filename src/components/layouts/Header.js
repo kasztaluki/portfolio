e6128b3c055f785ced25eca5c855d751a965e30e
kasztaluki <grabowski.lukasz.88@gmail.com>
2020-03-22 21:11:14 +0100

    Page in building content

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
-// import { NavLink, Link } from 'react-router-dom';
 import "../../styles/layouts/Header.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -9,57 +8,8 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import logoTitle from "../../images/logoTitle.PNG";
 
 class Header extends Component {
-  // eslint-disable-next-line
-  // constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       openMenu: false,
-  //       scroll: false
-  //     };
-  //   }
-
-  //   handleMenuClick = () => {
-  //     this.setState({ openMenu: !this.state.openMenu });
-  //   };
-
-  //   componentDidMount() {
-  //     window.addEventListener("scroll", this.handleScroll);
-  //   }
-
-  //   componentWillUnmount() {
-  //     window.removeEventListener("scroll", this.handleScroll);
-  //   }
-
-  //   handleScroll = event => {
-  //     if (window.scrollY === 0 && this.state.scroll === true) {
-  //       this.setState({ scroll: false });
-  //     } else if (window.scrollY !== 0 && this.state.scroll !== true) {
-  //       this.setState({ scroll: true });
-  //     }
-  //   };
 
   render() {
-    // const { openMenu, scroll } = this.state;
-
-    // const list = [
-    // { name: "Start", path: "/", exact: true },
-    // { name: "O mnie", path: "/about" },
-    // { name: "Umiejętności", path: "/skills" },
-    // { name: "Portfolio", path: "/portfolio" },
-    // { name: "Kontakt", path: "/contact" }
-    // ];
-
-    // const navigation = list.map(item => (
-    // <li className="menu_item" key={item.name}>
-    //     <NavLink
-    //     to={item.path}
-    //     exact={item.exact ? item.exact : false}
-    //     onClick={this.handleMenuClick}
-    //     >
-    //     {item.name}
-    //     </NavLink>
-    // </li>
-    // ));
 
     return (
       <header className="header">
@@ -95,14 +45,6 @@ class Header extends Component {
               </a>
             </div>
           </div>
-          <nav className="navigation_menu">
-            <div>Start</div>
-            <div>O mnie</div>
-            <div>Umiejętności</div>
-            <div>Projekty</div>
-            <div>Kontakt</div>
-            {/* <ul>navigation</ul> */}
-          </nav>
         </div>
       </header>
     );
