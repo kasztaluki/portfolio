0633ceb5bd9e9eed35b46bcef669ed387c4fb710
kasztaluki <grabowski.lukasz.88@gmail.com>
2020-04-27 20:29:59 +0200

    change icon.ico

--- src/components/layouts/Main.js
+++ src/components/layouts/Main.js
@@ -7,14 +7,14 @@ class Main extends Component {
     return (
       <div className="main_content">
         <div className="content">
-          <h1>Prace nad stroną WebDevChallenge.pl trwają!</h1>
+          <h1>Prace nad stroną <br/>WebDevChallenge.pl trwają!</h1>
           <p><span>Cześć, tu Łukasz!</span>Powiedziałem sobie &quot;CHALLENGE ACCEPTED&quot; i tworzę stronę poświęconą programowaniu. Mówiąc konkretniej historii mojej drogi do
             stania się zawodowym web developerem. W moim przypadku od zera do bohatera. Aktualnie studiuję na
             Politechnice Gdańskiej kierunek &quot;Programowanie i bazy danych&quot;, a swoją wiedzę rozwijam w technologiach:
             HTML, CSS, JavaScript, C++, Java, SQL. Dowiesz się tutaj kim jestem, zobaczysz moje projekty programistyczne
             oraz znajdziesz sposób aby się ze mną skontaktować. Już teraz zapraszam Cię do odwiedzenia mojego
             Githuba, Linkedina bądź do kontaktu mailowego albo telefonicznego:<br/><br/>
-            <span>mail: lukasz.grabowski.pg@gmail.com || tel. 791 964 564.</span><br/>
+            <span>mail: lukasz.grabowski.pg@gmail.com <br />|| tel. 791 964 564.</span><br/>
             Pracuję nad tym, aby strona pojawiła się tutaj jak najszybciej.</p>
           <h2>Keep calm, be patient, get back here later!</h2>
         </div>
