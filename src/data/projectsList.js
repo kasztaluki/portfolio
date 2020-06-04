fafae0ef66cb03066ba7b833c2e7ce2c6461bdc3
kasztaluki <grabowski.lukasz.88@gmail.com>
2020-06-04 23:33:39 +0200

    Add styles in ProjectItem

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -1,26 +1,26 @@
-
+// import ProjHangman from '../assets/images/ProjHangman.png';
 
 // eslint-disable-next-line import/prefer-default-export
 export const projectsList = [
     {
         name: 'Portfolio website',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
         tech: 'HTML5, CSS3, JS ES6, REACT',
     },
     {
-        name: 'Project two',
-        image: 'acb',
-        codeLink: 'cvd',
-        releaseLink: 'sas',
-        description: 'Projekt dotyczy czegoś tam itd itp',
-        tech: 'HTML5, CSS3, JS ES6, REACT',
+        name: 'Hangman Game',
+        image: 'https://picsum.photos/200/200', // image: ProjHangman,
+        codeLink: 'https://github.com/kasztaluki/hangman-game',
+        releaseLink: 'https://kasztaluki.github.io/hangman-game/',
+        description: 'Gra w wisielca. Prosty projekt zrealizowany w JavaScript, HTML5 oraz CSS3. Gra przechowuje hasła, które są losowane podczas odświeżania strony oraz po zakończeniu danej rozgrywki. Wybrane litery zmieniają kolor na zielony lub czerwony w zależności czy litera występuje w haśle oraz uruchamiają dźwięk wyboru. Po wskazaniu litery pole wyboru zostaje dezaktywowane.',
+        tech: 'HTML5, CSS3, JS ES6',
     },
     {
         name: 'Project three',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
@@ -28,7 +28,7 @@ export const projectsList = [
     },
     {
         name: 'Portfolio website',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
@@ -36,7 +36,7 @@ export const projectsList = [
     },
     {
         name: 'Project two',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
@@ -44,7 +44,7 @@ export const projectsList = [
     },
     {
         name: 'Project three',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
@@ -52,7 +52,7 @@ export const projectsList = [
     },
     {
         name: 'Portfolio website',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
@@ -60,7 +60,7 @@ export const projectsList = [
     },
     {
         name: 'Project two',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
@@ -68,7 +68,7 @@ export const projectsList = [
     },
     {
         name: 'Project three',
-        image: 'acb',
+        image: 'https://picsum.photos/200/200',
         codeLink: 'cvd',
         releaseLink: 'sas',
         description: 'Projekt dotyczy czegoś tam itd itp',
